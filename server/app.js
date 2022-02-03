2b52b25717272592dd75adbb5ecf7400d3fd89e8
john-v77 <john.vs11@hotmail.com>
2022-02-03 02:18:55 -0500

    connecting database

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 const app = express()
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
 
+
+
+//conection to Mongo DB
+mongoose.connect(MONGO_URI)
+        .then(console.log(`connected to database`), _=> app.listen(PORT))
+        .catch(err => {console.log(err)})
+
+        
 app.get('/', (req, res, next) => {
     res.send('Hello World!')
 })
-
-app.listen(3000)
