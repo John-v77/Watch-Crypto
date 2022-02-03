8e720bb72423558973b672d4eb6dad8c947f9cfe
john-v77 <john.vs11@hotmail.com>
2022-02-03 02:46:22 -0500

    graphql schema - Auth user

--- server/app.js
+++ server/app.js
@@ -1,19 +1,27 @@
 const express = require('express')
+
+
 require('dotenv').config()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-const app = express()
+const graphqlHttp = require('express-graphql')
+
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
 
+//import modules
+const graphqlSchema = require(`./graphQL/schema`)
 
+const app = express()
+app.use(bodyParser.json())
 
 //conection to Mongo DB
 mongoose.connect(MONGO_URI)
         .then(console.log(`connected to database`), _=> app.listen(PORT))
         .catch(err => {console.log(err)})
 
-        
+
 app.get('/', (req, res, next) => {
     res.send('Hello World!')
 })
